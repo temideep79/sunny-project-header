884e30ef8231d5fa6f963605ce22538d7d1b730b
temideep79 <69062515+temideep79@users.noreply.github.com>
2022-11-18 13:13:53 +0100

    Add files via upload

---
+++ SUNNYPROJ/js/index.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const hamburger_icon = hamburger.querySelector("span");
+const mobile_menu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger_icon.innerText =
+    hamburger_icon.innerText === "menu" ? "menu_open" : "menu";
+
+  mobile_menu.classList.toggle("is-open");
+});
